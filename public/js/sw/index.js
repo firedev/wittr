842bbc9f90edf28b0bfe32071ce3ea7f49405547
Nick Ostrovsky <nick@firedev.com>
2020-11-29 22:03:53 +0300

    Install Cache

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,4 +1,22 @@
-const version = 2
+const version = 1
+const urlsToCache = [
+  '/',
+  '/js/main.js',
+  '/css/main.css',
+  '/imgs/icon.png',
+  'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+  'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+]
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(`wittr-static-v${version}`)
+    .then(function(cache) {
+      return(cache.addAll(urlsToCache))
+    })
+  )
+})
+
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     fetch(event.request).then(function(response) {
